aeb635994b867d7595b29cde85a4fa362d0c8a96
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-01-05 12:11:51 +0900

    Initial commit

---
+++ routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  root to: "items#index"
+
+  # devise_for :users
+
+  resources :users, only: [:show] do
+    collection do
+      get "card"
+      get "selling"
+      get "selling_progress"
+      get "sold"
+      get "bought_progress"
+      get "bought_past"
+    end
+  end
+
+  resources :items  do
+    member do
+      get "purchase_confirmation"
+      post "purchase"
+    end
+  end
+  resources :categories, only: [:index, :show]
+  resource :cards, only: [:new, :create, :show, :update, :destroy]
+
+end
