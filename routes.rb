e9655ac15cb89bf62b05dcf69c4e54d4a9f7b76f
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-03-20 12:41:23 +0900

    change resources to resource

--- routes.rb
+++ routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # devise_for :users
 
-  resources :users, only: [:show] do
+  resource :users, only: [:show] do
     collection do
       get "card"
       get "selling"
